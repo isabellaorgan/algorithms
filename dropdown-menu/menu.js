96b3da8b32dbe9461558404f5afa655d66ec7641
Isabella Organ <isabellaorgan@gmail.com>
2018-09-20 12:32:35 -0700

    update spacing.

--- dropdown-menu/menu.js
+++ dropdown-menu/menu.js
@@ -1,7 +1,7 @@
  $(document).ready(function(){
-   $("#menu li").hover(function(){
-     $(".dropdown-menu", this).slideDown(100);
-   }, function(){
-     $(".dropdown-menu", this).stop().slideUp(100);
-   });
+    $("#menu li").hover(function(){
+        $(".dropdown-menu", this).slideDown(100);
+    }, function(){
+        $(".dropdown-menu", this).stop().slideUp(100);
+    });
  });
