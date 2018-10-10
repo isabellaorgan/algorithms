9a3396bdbe740bb665eac49be7300afbaf546cc2
Isabella Organ <isabellaorgan@gmail.com>
2018-10-10 14:00:06 -0700

    Adds a third solution which I like for brevity and readability, but the result may be heavy handed, so also a 4th solution which does not remove all characters that repeat, but only the instances of more than one.

--- removeDupeChars.js
+++ removeDupeChars.js
@@ -3,6 +3,7 @@
 // The question asks that you remove duplicate characters. 
 // Solution 1 works perfectly by finding
 // characters that are not dupes (unique) and logs only those into the console.
+// returns barn
 
 function findUniqueChars(string) {
 
@@ -19,8 +20,9 @@ function findUniqueChars(string) {
 console.log(findUniqueChars('baraban'));
 
 
-// Solution works as well, although I personally feel like the need for a callback in the filter
+// Solution 2 works as well, although I personally feel like the need for a callback in the filter
 // method is slightly less readable and easy to understand.
+// returns barn
 
 function removeDupeChars(string) {
     return string
@@ -32,3 +34,24 @@ function removeDupeChars(string) {
 }
 
 console.log(removeDupeChars('baraban'));
+
+// Solution 3 may be the winner as it is the most concise, readable and easily understandable, even with the filter method
+// and the callback. However, where the first two solutions remove ONLY the duplicate instance (so the second b and 2nd & 3rd a)
+// this solution will remove all characters that occur more than once.
+// returns rn
+
+var str = 'baraban'.split('');
+var rmDup = str.filter(function(val, i, str) {
+     return str.lastIndexOf(val) === str.indexOf(val);
+});
+console.log(rmDup.join(''));
+
+// Solution 4 refactors Solution 3 to only remove the duplicate instances (1 instance of b, 2 instances of a)
+// returns rban
+
+var str = 'baraban'.split('');
+var rmDup = str.filter(function(val, i, str) {
+     return str.lastIndexOf(val) == i;
+});
+console.log(rmDup.join(''));
+
