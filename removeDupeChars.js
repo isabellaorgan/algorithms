6e476b4c14c657c90392e49b09eff071aaece291
Isabella Organ <isabellaorgan@gmail.com>
2018-10-10 13:39:52 -0700

    Second solution, works the same, roughly the same complexity, but is it as easy to read and understand due to the use of the filter method which uses a callback?

--- removeDupeChars.js
+++ removeDupeChars.js
@@ -1,3 +1,9 @@
+// BofA interview question, via Glassdoor.
+
+// The question asks that you remove duplicate characters. 
+// Solution 1 works perfectly by finding
+// characters that are not dupes (unique) and logs only those into the console.
+
 function findUniqueChars(string) {
 
     var unique='';
@@ -12,6 +18,17 @@ function findUniqueChars(string) {
 
 console.log(findUniqueChars('baraban'));
 
-// BofA interview question, via Glassdoor.
-// The question asks that you remove duplicate characters, this solution works perfectly by finding
-// characters that are not dupes (unique) and logs only those into the console.
+
+// Solution works as well, although I personally feel like the need for a callback in the filter
+// method is slightly less readable and easy to understand.
+
+function removeDupeChars(string) {
+    return string
+    .split('')
+        .filter(function(item, pos, self) {
+          return self.indexOf(item) == pos;
+        })
+    .join('');
+}
+
+console.log(removeDupeChars('baraban'));
